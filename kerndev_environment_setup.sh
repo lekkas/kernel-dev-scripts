5956d6f1ceeda86722c38463405abce5f9283468
Kostas Lekkas <kostas@lekkas.io>
2015-07-25 20:29:23 +0300

    Do not recreate VM root fs image if file exists

--- kerndev_environment_setup.sh
+++ kerndev_environment_setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 source ./deps/kerndev-vars.sh
 source ./deps/kerndev-functions.sh
 
@@ -30,7 +32,7 @@ echo "## Adding .rc files into /home/"$USER" ##"
 runAs "$USER" "$(pwd)/createConfigFiles.sh"
 
 echo "## Cloning linux git repository into $LINUX_SOURCE_HOME"
-runAs "$USER" "git clone "$LINUX_GIT" "$LINUX_SOURCE_HOME""
+runAs "$USER" "git clone "$LINUX_GIT" "$LINUX_SOURCE_HOME"" || true
 
 #######################
 # VM Root Image Setup #
@@ -39,38 +41,37 @@ runAs "$USER" "git clone "$LINUX_GIT" "$LINUX_SOURCE_HOME""
 echo "## Creating Centos 7 VM root image"
 if [ -a "$ROOTFS_IMG" ];
 then
-  echo "## $ROOTFS_IMG already exists. Backing it up. ##"
-  mv $ROOTFS_IMG $ROOTFS_IMG.$(date +%s)
+  echo "## $ROOTFS_IMG already exists. ##"
+else
+  runAs "$USER" "qemu-img create -f raw "$ROOTFS_IMG" "$ROOTFS_SIZE""
+  runAs "$USER" "mkfs.ext4 "$ROOTFS_IMG""
+  mount -o loop "$ROOTFS_IMG" "$CHROOT"
+
+  # Initialize RPM database for VM root image
+  mkdir -p "$CHROOT/var/lib/rpm"
+  rpm --root="$CHROOT" --rebuilddb
+
+  # Download centos-release
+  push /tmp
+  wget "$CENTOS7_URL"
+  sudo rpm --root="$CHROOT" --nodeps -i "$CENTOS7_RPM"
+  # Cleanup
+  rm /tmp/"$CENTOS7_RPM"
+  pop
+
+  echo "## Install CentOS base on VM root image ##"
+  yum --installroot="$CHROOT" update
+  yum --installroot="$CHROOT" install -y yum
+  # Install dracut to create initramfs image
+  yum --installroot="$CHROOT" install -y dracut
+
+  # Make root passwordless for convenience.
+  sed -i '/^root/ { s/:x:/::/ }' "$CHROOT"/etc/passwd
+
+  # TODO : Add user into VM root image ?
+
+  # Cleanup and exit
+  unmount "$CHROOT"
 fi
-
-runAs "$USER" "qemu-img create -f raw "$ROOTFS_IMG" "$ROOTFS_SIZE""
-runAs "$USER" "mkfs.ext4 "$ROOTFS_IMG""
-mount -o loop "$ROOTFS_IMG" "$CHROOT"
-
-# Initialize RPM database for VM root image
-mkdir -p "$CHROOT/var/lib/rpm"
-rpm --root="$CHROOT" --rebuilddb
-
-# Download centos-release
-push /tmp
-wget "$CENTOS7_URL"
-sudo rpm --root="$CHROOT" --nodeps -i "$CENTOS7_RPM"
-# Cleanup
-rm /tmp/"$CENTOS7_RPM"
-pop
-
-echo "## Install CentOS base on VM root image ##"
-yum --installroot="$CHROOT" update
-yum --installroot="$CHROOT" install -y yum
-# Install dracut to create initramfs image
-yum --installroot="$CHROOT" install -y dracut
-
-# Make root passwordless for convenience.
-sed -i '/^root/ { s/:x:/::/ }' "$CHROOT"/etc/passwd
-
-# TODO : Add user into VM root image ?
-
-# Cleanup and exit
-unmount "$CHROOT"
 
 echo "## Done! ##"
