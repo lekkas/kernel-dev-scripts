1a0f9028c3de18fa2c21d3036f85e6e8c0ce17fb
Kostas Lekkas <kostas@lekkas.io>
2015-08-09 00:33:27 +0300

    Add usb host -> guest map & sudo execution

--- qemu_run.sh
+++ qemu_run.sh
@@ -7,7 +7,7 @@ source ./deps/kerndev-functions.sh
 # in CentOS 7
 export PATH=$PATH:/usr/libexec
 
-qemu-kvm -enable-kvm -nographic \
+sudo /usr/libexec/qemu-kvm -enable-kvm -nographic \
   -kernel "$KERNDEV_HOME/boot/bzImage" \
   -initrd "$KERNDEV_HOME/boot/initramfs.img" \
   -hda "$ROOTFS_IMG" \
@@ -15,7 +15,11 @@ qemu-kvm -enable-kvm -nographic \
   -append "root=/dev/sda rw console=ttyS0"
 # Add usb host
 # -usb -device usb-host,id=hostkbd,hostbus=3
+  -monitor telnet:127.0.0.1:1234,server,nowait \
+  -usb
 
+#  -usb -device usb-host,id=hostkbd,hostbus=3
+#  -usb -device usb-kbd,id=hostkbd,hostbus=3,hostaddr=5 \
 
 # In Qemu monitor (telnet localhost 1234):
   # Detach keyboard
